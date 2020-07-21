bb96ea22e8406c6e83f88f8a9d10c1b0f17c0701
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-21 10:33:04 +0100

    code modification for node app

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 const htmlDirectory = path.join(__dirname, '..') 
 
 app.use(express.static(htmlDirectory))
 
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000')
+app.listen(port,()=>{
+    console.log('server is up on port ' + port)
 })
