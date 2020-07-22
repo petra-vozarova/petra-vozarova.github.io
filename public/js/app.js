7515d321042ef567180e52ea543846f3257f8955
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-22 17:19:07 +0100

    adding quote-machine project

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ function myNumber (){
    document.getElementById('myNumber').innerHTML='My number is'
 }
 
-
+/*
 </div class='svg'>
 <svg width='100%' height='200'>
     <polygon points="2100,2100 0,200 2100,0" id='triangleDown'/>
